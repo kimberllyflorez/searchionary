e08fae509e746ff2c360d0e9f0a1ebc3c19794e8
kimberlly florez <kimberlly.florez@gmail.com>
2024-03-31 23:15:12 +1100

    implementation repository with use case

--- app/src/main/java/com/bimblystudios/searchionary/domain/usecases/SearchWordUseCase.kt
+++ app/src/main/java/com/bimblystudios/searchionary/domain/usecases/SearchWordUseCase.kt
@@ -1,4 +1,8 @@
 package com.bimblystudios.searchionary.domain.usecases
 
-class SearchWordUseCase {
+import com.bimblystudios.searchionary.data.model.Word
+import com.bimblystudios.searchionary.data.repositories.DictionaryRepository
+
+ class SearchWordUseCase(  private val searchWordRepository: DictionaryRepository) {
+    suspend operator fun  invoke(query: String): List<Word>? = searchWordRepository.searchWord(query)
 }
