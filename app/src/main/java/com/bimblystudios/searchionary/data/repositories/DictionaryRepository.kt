4c8e3fc4ae5edfbb5974acbe187ff021c1025b7e
kimberlly florez <kimberlly.florez@gmail.com>
2024-03-31 23:16:22 +1100

    implementation repository with use case

--- app/src/main/java/com/bimblystudios/searchionary/data/repositories/DictionaryRepository.kt
+++ app/src/main/java/com/bimblystudios/searchionary/data/repositories/DictionaryRepository.kt
@@ -3,7 +3,7 @@ package com.bimblystudios.searchionary.data.repositories
 import com.bimblystudios.searchionary.data.model.Word
 import com.bimblystudios.searchionary.data.remote.DictionaryEndpointFactory
 
-class DictionaryRepository (word: String,  private  val dictionaryEndpoint: DictionaryEndpointFactory){
+class DictionaryRepository (  private  val dictionaryEndpoint: DictionaryEndpointFactory){
 
 suspend  fun  searchWord(word: String): List<Word>? {
     if (word.isEmpty())return  null
